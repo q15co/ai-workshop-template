fab6c0b44c4de2997ff036d2c24f6a8a21351735
Alina <info@alina-nerlich.com>
2024-09-23 18:39:38 +0200

    video generation

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,3 +1,42 @@
-export const MyComposition = () => {
-  return null;
+import { AbsoluteFill, Img, Series, staticFile } from "remotion";
+import { z } from "zod";
+
+export const compSchema = z.object({
+  title: z.string(),
+  scenes: z.array(
+    z.object(
+      {
+        text: z.string(), 
+        image:z.string()}))
+
+})
+
+export const MyComposition = (props: z.infer<typeof compSchema>) => {
+  return (
+    <>
+  <AbsoluteFill
+  style={{
+    background: "#000",
+    justifyContent: "center",
+    color: "white",
+    alignItems: "center",
+    fontSize: 60,
+
+  }}>
+    <Series>
+      {props.scenes.map((scene) => (
+          <Series.Sequence durationInFrames={30} style={{
+            display: "flex",
+            flexDirection:"column",
+          }}>
+            <Img src={staticFile(scene.image)}/>
+            <p>{scene.text}</p>
+          </Series.Sequence>
+      ))}
+     
+    </Series>
+  </AbsoluteFill>
+    </>
+  )
+
 };
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,5 +1,6 @@
 import { Composition } from "remotion";
-import { MyComposition } from "./Composition";
+import { compSchema, MyComposition } from "./Composition";
+import defaultProps from "./data.json"
 
 export const RemotionRoot: React.FC = () => {
   return (
@@ -7,10 +8,12 @@ export const RemotionRoot: React.FC = () => {
       <Composition
         id="MyComp"
         component={MyComposition}
-        durationInFrames={60}
+        durationInFrames={240}
         fps={30}
         width={1080}
         height={1920}
+        schema={compSchema}
+        defaultProps={defaultProps}
       />
     </>
   );
